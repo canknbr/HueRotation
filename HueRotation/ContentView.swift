087c2a228cf5e9ddd8db48a39ef0f6473b7b582a
canknbr <cankanbur1@gmail.com>
2023-04-03 15:46:13 +0300

    added sample

--- HueRotation/ContentView.swift
+++ HueRotation/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var hueRotation = false
+    var backgrounds = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13"].shuffled()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader {
+            geo in
+            BackgroundScroll(imageCount: backgrounds.count) {
+                ForEach(0 ..< backgrounds.count) { number in
+                    Image(backgrounds[number]).resizable().scaledToFill()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
+                }
+            }.hueRotation(.degrees(hueRotation ? 50 : 500))
+                .animation(Animation.easeInOut(duration: 5).repeatForever(autoreverses: true), value: hueRotation)
+                .onAppear(){
+                    hueRotation.toggle()
+                }
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct BackgroundScroll<ViewContent: View>: View {
+    var imageCount: Int
+    var contentContainer: ViewContent
+    @State var workingIndex: Int = 0
+    let timer = Timer.publish(every: 5, on: .main, in: .default).autoconnect()
+
+    init(imageCount: Int, @ViewBuilder content: () -> ViewContent) {
+        self.imageCount = imageCount
+        contentContainer = content()
+    }
+
+    var body: some View {
+        GeometryReader {
+            geo in
+            ZStack(alignment: .bottom) {
+                HStack(spacing: 0) {
+                    contentContainer
+                }.frame(width: geo.size.width, height: geo.size.height, alignment: .leading)
+                    .offset(x: CGFloat(workingIndex) * -geo.size.width, y: 0)
+                    .animation(.easeInOut)
+                    .onReceive(timer) { _ in
+                        workingIndex = (workingIndex + 1) % (imageCount == 0 ? 1 : imageCount)
+                    }
+
+                HStack(spacing: 7) {
+                    ForEach(0 ..< imageCount) { index in
+                        Circle().frame(width: index == workingIndex ? 13 : 9, height: index == workingIndex ? 13 : 9)
+                            .foregroundColor(index == workingIndex ? .white : .gray)
+                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                            .padding(.bottom, 20)
+                            .animation(.easeInOut)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
